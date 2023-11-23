86cdd6f6ddc2ff494df4ee924f5ef4db49327cf1
AndreZardo <andrezardo@gmail.com>
2023-11-23 19:24:27 -0300

    primeiro codigo

--- forca.py
+++ forca.py
@@ -1,1 +1,15 @@
-print("teste")
+def jogar();
+    print("Jogo da Forca:")
+
+    palavra_secreta = "banana"
+    
+    enforcou = False
+    acertou = False
+    
+    while (not enforcou or not acertou)
+        print("jogando..")
+
+    print("Fim do jogo!")
+
+if(__name__ == "__main__"):
+    jogar()
